d9d2411bbfd9a9104fa8cf6fcdb3a76bfae0e665
Yadhukrishna <yadhukishore37@gmail.com>
2024-01-30 12:24:36 +0530

    db

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const dbConnect = () => {
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL).then(() => {
+mongoose.connect(process.env.MONGO_ATLAS_URL).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Error connecting to MongoDB:', err);
